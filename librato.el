067fa0b10f8f274f8541007241d59e1b0841cadd
Jaime Fournier <jaimef@linbsd.org>
2014-08-11 22:00:56 -0700

    cleanup. fixed base64 inserting \n.

--- librato.el
+++ librato.el
@@ -37,26 +37,86 @@
 ;;; Code:
 
 (require 'web)
-(require 'request)
 (require 'json)
 
-(setq librato_basic_auth (concat "Basic " (base64-encode-string (concat librato-email ":" librato-key))))
+(setq librato_basic_auth (concat "Basic " (base64-encode-string (concat librato-email ":" librato-key) 1)))
 
 (defun get-librato-metrics ()
   (interactive)
   (librato-get-metrics "https://metrics-api.librato.com/v1/metrics"))
 
+(defun librato-metrics-query (query)
+  (interactive "sLibrato Metric?:")
+  (librato-get-metrics (format "https://metrics-api.librato.com/v1/metrics?name=%s" query)))
+
+(defun test-librato-metrics ()
+  (interactive)
+  (librato-get-metrics (format "http://localhost:4567/get")))
+
 (defun librato-get-metrics (uri)
   (let ((data `(("Authorization" . ,librato_basic_auth))))
     (web-http-get
      (lambda (httpc header my-data)
-       (with-output-to-temp-buffer "*librato*"
-         (switch-to-buffer-other-window "*librato*")
-         (mapcar #'librato-print-message (json-read-from-string my-data))))
-     ;;(mapcar #'librato-print-metrics (cdr (assoc 'collectors (json-read-from-string my-data))))))
+       (with-output-to-temp-buffer "*librato-metrics*"
+         (switch-to-buffer-other-window "*librato-metrics*")
+	 (mapcar #'librato-print-metrics (cdr (assoc 'metrics (json-read-from-string my-data))))))
      :url uri
      :extra-headers data
      )))
 
+;; (defun librato-get-dashboards ()
+;;   (let ((uri "https://metrics-api.librato.com/v1/dashboards"))
+;;     (librato-get-generic uri function
+
+
+(defun librato-get-dashboards (uri)
+  (let ((data `(("Authorization" . ,librato_basic_auth))))
+    (web-http-get
+     (lambda (httpc header my-data)
+       (with-output-to-temp-buffer "*librato-metrics*"
+         (switch-to-buffer-other-window "*librato-metrics*")
+	 (mapcar #'librato-print-metrics (cdr (assoc 'metrics (json-read-from-string my-data))))))
+     :url uri
+     :extra-headers data
+     )))
+
+(defun librato-print-metrics (element)
+  (let*
+      ((period (cdr (assoc 'period element)))
+       (description (cdr (assoc 'description element)))
+       (attributes (cdr (assoc 'attributes element)))
+       (aggregate (cdr (assoc 'aggregate attributes)))
+       (summarize_function (cdr (assoc 'summarize_function attributes)))
+       (display_stacked (cdr (assoc 'display_stacked attributes)))
+       (gap_detection (cdr (assoc 'gap_detection attributes)))
+       (created_by_ua (cdr (assoc 'created_by_ua attributes)))
+       (type (cdr (assoc 'type element)))
+       (display_name (cdr (assoc 'display_name element)))
+       (name (cdr (assoc 'name element))))
+    (insert (propertize (format "Name:%s " name) 'face '(:foreground "black")))
+    (insert (propertize (format "Dispay:%s " display_name) 'face '(:foreground "darkgreen")))
+    (insert (propertize (format "Descrip:%s " description) 'face '(:foreground "darkblue")))
+    (insert (propertize (format "Aggregate?:%s " aggregate) 'face '(:foreground "darkpink")))
+    (insert (propertize (format "Summarize?:%s " summarize_function) 'face '(:foreground "purple")))
+    (insert (propertize (format "Stacked?:%s " display_stacked) 'face '(:foreground "darkgrey")))
+    (insert (propertize (format "Gap Detect?:%s " gap_detection) 'face '(:foreground "purple")))
+    (insert (propertize (format "Created By: %s " created_by_ua) 'face '(:foreground "purple")))
+    (insert (propertize (format "Type: %s " type) 'face '(:foreground "purple")))
+    (princ "\n")))
+
 (defun librato-print-message (element)
-  (message "SSS: %s" element) )
+  (message "SSS: %s" element))
+
+(defun librato-create-link-for-metric (color title id)
+  "Insert clickable string inside a buffer"
+  (lexical-let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<RET>")
+      #'(lambda (e) (interactive "p") (librato-get-metrics id)))
+    (define-key map (kbd "<down-mouse-1>")
+      #'(lambda (e) (interactive "p") (librato-get-metrics id)))
+    (insert
+     (propertize
+      title
+      'face '(:foreground color)
+      'keymap map
+      'mouse-face 'highlight))))
