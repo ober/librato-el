5f446389a1875e3ead1d765fd11d4f5421b6e125
Jaime Fournier <jaimef@xxxxxxxx.local>
2014-08-14 20:37:18 -0700

    add spark lines.

--- librato.el
+++ librato.el
@@ -49,6 +49,10 @@
   (interactive "sLibrato Metric?:")
   (librato-get-metrics (format "https://metrics-api.librato.com/v1/metrics?name=%s" query)))
 
+(defun librato-metrics-query-by-source (query source)
+  (interactive "sLibrato Metric?:\nsSource:")
+  (librato-get-metrics-by-func (format "https://metrics-api.librato.com/v1/metrics?name=%s&source=%s" query source) 'librato-print-message))
+
 (defun test-librato-metrics ()
   (interactive)
   (librato-get-metrics (format "http://localhost:4567/get")))
@@ -64,10 +68,20 @@
      :extra-headers data
      )))
 
+(defun librato-get-metrics-by-func (uri func)
+  (let ((data `(("Authorization" . ,librato_basic_auth))))
+    (web-http-get
+     (lambda (httpc header my-data)
+       (with-output-to-temp-buffer "*librato-metrics*"
+         (switch-to-buffer-other-window "*librato-metrics*")
+	 (mapcar #'(intern func) (cdr (assoc 'metrics (json-read-from-string my-data))))))
+     :url uri
+     :extra-headers data
+     )))
+
 ;; (defun librato-get-dashboards ()
 ;;   (let ((uri "https://metrics-api.librato.com/v1/dashboards"))
 ;;     (librato-get-generic uri function
-
 
 (defun librato-get-dashboards (uri)
   (let ((data `(("Authorization" . ,librato_basic_auth))))
@@ -76,6 +90,19 @@
        (with-output-to-temp-buffer "*librato-metrics*"
          (switch-to-buffer-other-window "*librato-metrics*")
 	 (mapcar #'librato-print-metrics (cdr (assoc 'metrics (json-read-from-string my-data))))))
+     :url uri
+     :extra-headers data
+     )))
+
+(defun librato-fetch-metric-data (metric)
+  (lexical-let* ((data `(("Authorization" . ,librato_basic_auth)))
+		 (output-buffer (format "*librato-%s-data*" metric))
+		 (uri (format "https://metrics-api.librato.com/v1/metrics/%s?count=100&resolution=60" metric )))
+    (web-http-get
+     (lambda (httpc header my-data)
+       (with-output-to-temp-buffer output-buffer
+         (switch-to-buffer-other-window output-buffer)
+	 (mapcar #'librato-print-metrics-data (cdr (assoc 'measurements (json-read-from-string my-data))))))
      :url uri
      :extra-headers data
      )))
@@ -93,10 +120,11 @@
        (type (cdr (assoc 'type element)))
        (display_name (cdr (assoc 'display_name element)))
        (name (cdr (assoc 'name element))))
-    (insert (propertize (format "Name:%s " name) 'face '(:foreground "black")))
-    (insert (propertize (format "Dispay:%s " display_name) 'face '(:foreground "darkgreen")))
+    (librato-create-link-for-metric "blue" name name)
+    ;;(insert (propertize (format "Name:%s " name) 'face '(:foreground "black")))
+    (insert (propertize (format " Dispay:%s " display_name) 'face '(:foreground "darkgreen")))
     (insert (propertize (format "Descrip:%s " description) 'face '(:foreground "darkblue")))
-    (insert (propertize (format "Aggregate?:%s " aggregate) 'face '(:foreground "darkpink")))
+    (insert (propertize (format "Aggregate?:%s " aggregate) 'face '(:foreground "pink")))
     (insert (propertize (format "Summarize?:%s " summarize_function) 'face '(:foreground "purple")))
     (insert (propertize (format "Stacked?:%s " display_stacked) 'face '(:foreground "darkgrey")))
     (insert (propertize (format "Gap Detect?:%s " gap_detection) 'face '(:foreground "purple")))
@@ -104,19 +132,52 @@
     (insert (propertize (format "Type: %s " type) 'face '(:foreground "purple")))
     (princ "\n")))
 
+
+(defun librato-print-metrics-data (element)
+  (let ((source (car element))
+	(data (cdr element))
+	(i 0)
+	(deltas (list)))
+    (insert (propertize (format "%s " source 'face '(:foreground "darkgreen"))))
+    ;;(insert (propertize (format "%s " (cdr element) 'face '(:foreground "darkgreen"))))
+    (while (< i (length data))
+      (let* ((datum (elt data i))
+	     (delta (cdr (assoc 'delta datum)))
+	     (measure_time (cdr (assoc 'measure_time datum)))
+	     (value (cdr (assoc 'value datum))))
+
+	(insert (format " measure_time:%s delta:%s" measure_time delta))
+
+	(nconc deltas (list delta)))
+	(setq i (1+ i))))
+	;;(insert "||| size:%s type:%s raw:%s" (length data-line) (type-of data-line) data-line )
+	;;(insert (format "%s" (apply 'spark data-line)))
+	(insert (format "%s" data-line))
+      (princ "\n")))
+
+
 (defun librato-print-message (element)
   (message "SSS: %s" element))
 
 (defun librato-create-link-for-metric (color title id)
   "Insert clickable string inside a buffer"
-  (lexical-let ((map (make-sparse-keymap)))
+  (lexical-let ((color color)
+		(map (make-sparse-keymap)))
     (define-key map (kbd "<RET>")
-      #'(lambda (e) (interactive "p") (librato-get-metrics id)))
+      #'(lambda (e) (interactive "p") (librato-fetch-metric-data id)))
     (define-key map (kbd "<down-mouse-1>")
-      #'(lambda (e) (interactive "p") (librato-get-metrics id)))
+      #'(lambda (e) (interactive "p") (librato-fetch-metric-data id)))
     (insert
      (propertize
       title
-      'face '(:foreground color)
+      'face '(:foreground "blue")
       'keymap map
       'mouse-face 'highlight))))
+
+
+(defun spark (&rest args)
+   (let* ((minimum (apply #'min args))
+          (maximum (apply #'max args))
+          (range (float (- maximum minimum)))
+          (sparks '("▁" "▂" "▃" "▄" "▅" "▆" "▇" "█")))
+     (mapconcat (lambda (n) (nth (floor (* (/ n range) 7)) sparks)) args " ")))
